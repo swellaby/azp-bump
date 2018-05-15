f4a23993942b7fd7407ea7672d5e1520b359d915
Caleb Cartwright <caleb.cartwright@outlook.com>
2018-05-14 21:58:43 -0500

    added github template files. added barebones CLI implementation and setup unit tests for bin file

--- bin/vsts-bump.js
+++ bin/vsts-bump.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
-
 'use strict';
 
-throw new Error('Not yet implemented');
+const program = require('commander');
+
+// const index = require('../lib/index');
+const packageJson = require('../package.json');
+
+program
+    .version(packageJson.version)
+    .usage('<files> [options...]')
+    .option('-t, --type [type]', 'the bump version type')
+    .option('-i, --indent [indent]', 'the indent to use')
+    .parse(process.argv);
+
+const bump = () => {
+    const opts = program.opts();
+    console.log('Args: ' + program.args);
+    console.log('Release Type: ' + opts['type']);
+    console.log('Indent Type: ' + opts['indent']);
+    // return index.bumpWrite(program.args, opts);
+};
+
+bump();
+
+module.exports = {
+    bump: bump
+};
--- test/unit/bin/vsts-bump.js
+++ test/unit/bin/vsts-bump.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const assert = require('chai').assert;
+const commander = require('commander');
+const sinon = require('sinon');
+const index = require('../../../lib/index');
+const packageJson = require('../../../package.json');
+
+suite('bin/vsts-bump Suite:', () => {
+    const sandbox = sinon.createSandbox();
+    const commanderVersionSpy = sinon.spy(commander, 'version');
+    const commanderUsageSpy = sinon.spy(commander, 'usage');
+    const commanderOptionSpy = sinon.spy(commander, 'option');
+    const commanderParseStub = sinon.stub(commander, 'parse');
+    let indexBumpWriteStub;
+    let cli;
+
+    setup(() => {
+        indexBumpWriteStub = sandbox.stub(index, 'bumpWrite');
+        cli = require('../../../bin/vsts-bump');
+    });
+
+    teardown(() => {
+        sandbox.restore();
+    });
+
+    test('Should correctly set version', () => {
+        assert.isTrue(commanderVersionSpy.calledWith(packageJson.version));
+    });
+
+    test('Should correctly set usage', () => {
+        assert.isTrue(commanderUsageSpy.calledWith('<files> [options...]'));
+    });
+
+    test('Should correctly set type option', () => {
+        assert.isTrue(commanderOptionSpy.calledWith('-t, --type [type]', 'the bump version type'));
+    });
+
+    test('Should correctly set indent option', () => {
+        assert.isTrue(commanderOptionSpy.calledWith('-i, --indent [indent]', 'the indent to use'));
+    });
+
+    test('Should call parse with process.argv', () => {
+        assert.isTrue(commanderParseStub.calledWith(process.argv));
+    });
+});
